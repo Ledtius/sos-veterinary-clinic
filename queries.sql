4f7bdbe505ee268a4b4d173c356d01bf1ca32df7
Calet Devia <caletdevia@gmail.com>
2026-04-17 13:02:00 -0500

    Adding name to the constraints

--- queries.sql
+++ queries.sql
@@ -1,53 +1,73 @@
 ## Core People
 
 CREATE TABLE document_type(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL UNIQUE
+  id SERIAL,
+  name VARCHAR(100) NOT NULL,
+  CONSTRAINT pk_document_type PRIMARY KEY (id),
+  CONSTRAINT uq_document_type_name UNIQUE (name)
 );
 
 CREATE TABLE personal_data (
-  id SERIAL PRIMARY KEY,
-  document_type_id INT NOT NULL REFERENCES document_type(id),
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
-  document_number VARCHAR(20) NOT NULL UNIQUE,
-  birth_date DATE NOT NULL,
-  sex VARCHAR(10) NOT NULL CHECK(sex IN('Male', 'Female', 'Other')),
-  phone_number VARCHAR(30) NOT NULL,
-  address VARCHAR(255) NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ
+    id SERIAL,
+    document_type_id INT NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    document_number VARCHAR(20) NOT NULL,
+    birth_date DATE NOT NULL,
+    sex VARCHAR(10) NOT NULL,
+    phone_number VARCHAR(30) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ,
+    CONSTRAINT pk_personal_data PRIMARY KEY (id),
+    CONSTRAINT fk_personal_data_document_type FOREIGN KEY (document_type_id) REFERENCES document_type (id),
+    CONSTRAINT uq_personal_data_document_number UNIQUE (document_number),
+    CONSTRAINT chk_personal_data_sex CHECK (
+        sex IN ('Male', 'Female', 'Other')
+    ) CONSTRAINT uq_personal_data_document UNIQUE (
+        document_type_id,
+        document_number
+    )
 );
 
-CREATE TABLE owners(
-  id SERIAL PRIMARY KEY,
-  personal_data_id INT NOT NULL UNIQUE REFERENCES personal_data(id),
-  profile_image_id INT NOT NULL REFERENCES profile_images(id),
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ
+CREATE TABLE owners (
+    id SERIAL,
+    personal_data_id INT NOT NULL,
+    profile_image_id INT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ,
+    CONSTRAINT pk_owners PRIMARY KEY (id),
+    CONSTRAINT fk_uq_owners_personal_data FOREIGN KEY (personal_data_id) REFERENCES personal_data (id),
+    CONSTRAINT uq_owner_personal_data UNIQUE (personal_data_id)
 );
 
 CREATE TABLE staff (
-  id SERIAL PRIMARY KEY,
-  personal_data_id INT NOT NULL UNIQUE REFERENCES personal_data(id),
-  role_id INT NOT NULL REFERENCES roles(id),
-  auth_user_id INT NOT NULL UNIQUE REFERENCES auth_users(id),
-  profile_image_id INT NOT NULL REFERENCES profile_images(id),
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ
+    id SERIAL,
+    personal_data_id INT NOT NULL,
+    role_id INT NOT NULL,
+    auth_user_id INT NOT NULL,
+    profile_image_id INT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ,
+    CONSTRAINT pk_staff PRIMARY KEY (id),
+    CONSTRAINT fk_staff_personal_data FOREIGN KEY (personal_data_id) REFERENCES personal_data (id),
+    CONSTRAINT uq_staff_personal_data UNIQUE (personal_data_id),
+    CONSTRAINT fk_staff_role FOREIGN KEY (role_id) REFERENCES roles (id),
+    CONSTRAINT fk_staff_auth_user FOREIGN KEY (auth_user_id) REFERENCES auth_users (id),
+    CONSTRAINT uq_staff_auth_user UNIQUE (auth_user_id),
+    CONSTRAINT fk_staff_profile_image FOREIGN KEY (profile_image_id) REFERENCES profile_images (id)
 );
 
 CREATE TABLE auth_users (
-  id SERIAL PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password_hash VARCHAR(255) NOT NULL
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE roles (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(150) NOT NULL UNIQUE
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL UNIQUE
 );
-
 
 ## Pets
 
@@ -56,37 +76,38 @@ CREATE TABLE species(
   name VARCHAR(100) NOT NULL UNIQUE
 );
 
-CREATE TABLE breeds(
-  id SERIAL PRIMARY KEY,
-  species_id INT NOT NULL REFERENCES species(id),
-  name VARCHAR(100) NOT NULL,
-  UNIQUE(species_id, name)
+CREATE TABLE breeds (
+    id SERIAL PRIMARY KEY,
+    species_id INT NOT NULL REFERENCES species (id),
+    name VARCHAR(100) NOT NULL,
+    UNIQUE (species_id, name)
 );
 
 CREATE TABLE pets (
-  id SERIAL PRIMARY KEY,
-  species_id INT NOT NULL REFERENCES species(id),
-  breed_id INT REFERENCES breeds(id),
-  profile_image_id INT NOT NULL REFERENCES profile_images(id),
-  name VARCHAR(50) NOT NULL,
-  weight NUMERIC(6,2) NOT NULL,
-  sex VARCHAR(10) NOT NULL CHECK(sex IN('Male', 'Female')),
-  description TEXT,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ
+    id SERIAL PRIMARY KEY,
+    species_id INT NOT NULL REFERENCES species (id),
+    breed_id INT REFERENCES breeds (id),
+    profile_image_id INT NOT NULL REFERENCES profile_images (id),
+    name VARCHAR(50) NOT NULL,
+    weight NUMERIC(6, 2) NOT NULL,
+    sex VARCHAR(10) NOT NULL CHECK (sex IN ('Male', 'Female')),
+    description TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ
 );
 
-CREATE TABLE owners_pets(
-  id SERIAL PRIMARY KEY,
-  owner_id INT NOT NULL REFERENCES owners(id),
-  pet_id INT NOT NULL REFERENCES pets(id),
-  start_date DATE NOT NULL,
-  is_primary BOOLEAN NOT NULL DEFAULT false,
-  UNIQUE (owner_id, pet_id)
+CREATE TABLE owners_pets (
+    id SERIAL PRIMARY KEY,
+    owner_id INT NOT NULL REFERENCES owners (id),
+    pet_id INT NOT NULL REFERENCES pets (id),
+    start_date DATE NOT NULL,
+    is_primary BOOLEAN NOT NULL DEFAULT false,
+    UNIQUE (owner_id, pet_id)
 );
 
-CREATE UNIQUE INDEX unique_primary_owner_per_pet ON owners_pets(pet_id) WHERE is_primary = true;
-
+CREATE UNIQUE INDEX unique_primary_owner_per_pet ON owners_pets (pet_id)
+WHERE
+    is_primary = true;
 
 ## Media
 
@@ -98,7 +119,6 @@ CREATE TABLE profile_images (
 );
 
 CREATE UNIQUE INDEX unique_default_per_type ON profile_images(type) WHERE is_default = true;
-
 
 ## Appointments
 
@@ -117,19 +137,25 @@ CREATE TABLE appointments (
 );
 
 CREATE TABLE appointment_status (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(20)  NOT NULL CHECK(name IN('Pending', 'In Progress', 'Completed')) UNIQUE
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL CHECK (
+        name IN (
+            'Pending',
+            'In Progress',
+            'Completed'
+        )
+    ) UNIQUE
 );
 
-CREATE TABLE medical_records(
-  id SERIAL PRIMARY KEY,
-  appointment_id INT NOT NULL REFERENCES appointments(id) UNIQUE,
-  staff_id INT NOT NULL REFERENCES staff(id),
-  diagnosis TEXT NOT NULL,
-  treatment TEXT,
-  notes TEXT,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ
+CREATE TABLE medical_records (
+    id SERIAL PRIMARY KEY,
+    appointment_id INT NOT NULL REFERENCES appointments (id) UNIQUE,
+    staff_id INT NOT NULL REFERENCES staff (id),
+    diagnosis TEXT NOT NULL,
+    treatment TEXT,
+    notes TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ
 );
 
 ## Services
@@ -139,9 +165,9 @@ CREATE TABLE services(
   name VARCHAR(150) NOT NULL UNIQUE
 );
 
-CREATE TABLE categories(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(150) NOT NULL UNIQUE
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL UNIQUE
 );
 
 ## Notifications
@@ -152,11 +178,11 @@ CREATE TABLE notifications(
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE notifications_staff(
-  id SERIAL PRIMARY KEY,
-  notification_id INT NOT NULL REFERENCES notifications(id),
-  staff_id INT NOT NULL REFERENCES staff(id),
-  UNIQUE (notification_id, staff_id)
+CREATE TABLE notifications_staff (
+    id SERIAL PRIMARY KEY,
+    notification_id INT NOT NULL REFERENCES notifications (id),
+    staff_id INT NOT NULL REFERENCES staff (id),
+    UNIQUE (notification_id, staff_id)
 );
 
 ## Contact/Forms
@@ -168,17 +194,22 @@ CREATE TABLE form_contact_info(
   phone VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE form_messages(
-  id SERIAL PRIMARY KEY,
-  form_message_status_id INT NOT NULL REFERENCES form_message_status(id),
-  form_contact_info_id INT NOT NULL REFERENCES form_contact_info(id),
-  assigned_staff_id INT NOT NULL REFERENCES staff(id),
-  content TEXT NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+CREATE TABLE form_messages (
+    id SERIAL PRIMARY KEY,
+    form_message_status_id INT NOT NULL REFERENCES form_message_status (id),
+    form_contact_info_id INT NOT NULL REFERENCES form_contact_info (id),
+    assigned_staff_id INT NOT NULL REFERENCES staff (id),
+    content TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE form_message_status(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(20) NOT NULL CHECK(name IN('Unread', 'Viewed', 'Responded')) UNIQUE
+CREATE TABLE form_message_status (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL CHECK (
+        name IN (
+            'Unread',
+            'Viewed',
+            'Responded'
+        )
+    ) UNIQUE
 );
-
