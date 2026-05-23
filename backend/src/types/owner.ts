b9a5dd7e63b0b48bbd2cbae5e508192b8b032651
Calet Devia <caletdevia@gmail.com>
2026-05-23 14:23:10 -0500

    Changed the interfaces to be clearer, and I am continuing to work on the Image.utils.ts

--- backend/src/types/owner.ts
+++ backend/src/types/owner.ts
@@ -1,13 +1,14 @@
-export interface DocumentTypePost {
+/* RB = Response Body */
+export interface DocumentTypeRBPost {
   document_type_id: 1 | 2;
 }
 
-export interface ProfileImagePost {
+export interface ProfileImageRBPost {
   url: string;
 }
 
-export interface PersonalDataPost {
-  document_type: DocumentTypePost;
+export interface PersonalDataRBPost {
+  document_type: DocumentTypeRBPost;
   first_name: string;
   last_name: string;
   document_number: string;
@@ -17,7 +18,12 @@ export interface PersonalDataPost {
   address: string;
 }
 
+export interface OwnerRBPost {
+  personal_data: PersonalDataRBPost;
+  profile_image?: ProfileImageRBPost;
+}
+
 export interface OwnerPost {
-  personal_data: PersonalDataPost;
-  profile_image?: ProfileImagePost;
+  personal_data_id: number;
+  profile_image_id? : number;
 }
--- backend/src/utils/image.util.ts
+++ backend/src/utils/image.util.ts
@@ -1,15 +1,71 @@
-import type { Response } from "express";
+import type { Response, Request } from "express";
+import type { OwnerPost, OwnerRBPost } from "../types/owner";
+
+import { prismaClient } from "../lib/prisma";
+import type {
+  owners,
+  personal_data,
+  pets,
+  profile_images,
+  staff,
+} from "@prisma/client";
 
 type PostActionImage = "NO URL VALUE" | "URL VALUE" | "EMPTY URL VALUE";
 
-interface PostActionImageObject {
+type EntityName = "Owner" | "Pet" | "Staff";
+
+type EntityPD = owners | staff;
+
+interface ActionImageObject {
   action: PostActionImage;
   urlValue?: string;
 }
 
+export const postPersonalData = async (
+  req: Request,
+  imageValue: (urlValue: string | null | undefined) => ActionImageObject,
+) => {
+  const ownerBodyData: OwnerRBPost = req.body;
+
+  const { personal_data, profile_image } = ownerBodyData;
+
+  const { url } = profile_image ?? {};
+
+  const {
+    document_type,
+    first_name,
+    last_name,
+    document_number,
+    birth_date,
+    sex,
+    phone_number,
+    address,
+  } = personal_data;
+
+  const { document_type_id } = document_type;
+
+  const newPersonalData: personal_data =
+    await prismaClient.personal_data.create({
+      data: {
+        document_type_id,
+        first_name,
+        last_name,
+        document_number,
+        birth_date: new Date(birth_date),
+        sex,
+        phone_number,
+        address,
+      },
+    });
+
+  const imgValueObj: ActionImageObject = imageValue(url);
+
+  return { imgValueObj, newPersonalData };
+};
+
 export const imageValue = (
   urlValue: string | null | undefined,
-): PostActionImageObject => {
+): ActionImageObject => {
   if (!urlValue) return { action: "NO URL VALUE" };
   else {
     if (!urlValue.trim()) return { action: "EMPTY URL VALUE" };
@@ -17,6 +73,53 @@ export const imageValue = (
   }
 };
 
-export const postImage = (res: Response, imageValue: PostActionImageObject) => {
+export const postImage = async (
+  imageValue: ActionImageObject,
+  entityName: EntityName,
+  newPersonalData: personal_data,
+) => {
   const { action, urlValue } = imageValue;
+
+  let switchDefault: boolean = true;
+
+  if (action !== "URL VALUE") {
+    const defaultImage = await prismaClient.profile_images.findUnique({
+      where: { type: entityName, is_default: switchDefault },
+    });
+
+    postNewEntity(entityName, defaultImage as profile_images, newPersonalData);
+  } else {
+    const newImage = await prismaClient.profile_images.create({
+      data: {
+        url: urlValue as string,
+        type: entityName,
+        is_default: !switchDefault,
+      },
+    });
+
+    postNewEntity(entityName, newImage, newPersonalData);
+  }
+};
+
+const postNewEntity = async (
+  entityName: EntityName,
+  switchImage: profile_images,
+  newPersonalData: personal_data,
+) => {
+  let newEntity;
+
+  switch (entityName) {
+    case "Owner":
+      return (newEntity = prismaClient.owners.create({
+        data: {
+          personal_data_id: newPersonalData?.id,
+          profile_image_id: switchImage?.id,
+        },
+      }));
+
+    case "Staff":
+      return (newEntity = null);
+    default:
+      break;
+  }
 };
